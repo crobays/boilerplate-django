2dad5140ceeb7635374203b55727eb19c3264875
Maikel Vlasman <maikel@userex.nl>
2015-08-21 14:50:09 +0000

    fix template path in urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -6,7 +6,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     # Examples:
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
