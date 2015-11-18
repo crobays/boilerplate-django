0e8953841c79ef63de1bbf413aed78f68b1db957
Maikel Vlasman <maikel@mediamoose.nl>
2015-11-18 12:29:51 +0100

    add main default env vars

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -9,5 +9,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault('APP_NAME', 'main')
+os.environ.setdefault('APPLICATION_ENV', 'production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{}.settings.{}'.format(os.environ.get('APP_NAME'), os.environ.get('APPLICATION_ENV'))
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- src/manage.py
+++ src/manage.py
@@ -2,6 +2,10 @@
 import os
 import sys
 
+os.environ.setdefault('APP_NAME', 'main')
+os.environ.setdefault('APPLICATION_ENV', 'production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{}.settings.{}'.format(os.environ.get('APP_NAME'), os.environ.get('APPLICATION_ENV')))
+
 if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
